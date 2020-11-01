43a3ad81b7196b67692cac2588858da6ac52473a
salvacarrion <salva.carrion@outlook.com>
2020-11-01 01:57:27 +0100

    Add beam search + attention tracking

--- seq2seq/models/s2s_6_transfomer.py
+++ seq2seq/models/s2s_6_transfomer.py
@@ -302,77 +302,6 @@ class Seq2Seq(nn.Module):
 
         return output, attention
 
-    def _translate_sentence(self, src, max_trg_len=50):
-        # Single sentence ("unlimited length")
-
-        # Get special indices
-        sos_idx = self.trg_field.vocab.stoi[self.trg_field.init_token]
-        eos_idx = self.trg_field.vocab.stoi[self.trg_field.eos_token]
-
-        # Build source mask
-        src_mask = self.make_src_mask(src)
-
-        # Run encoder
-        with torch.no_grad():
-            enc_src = self.encoder(src, src_mask)
-
-        # Set fist word (<sos>)
-        trg_indexes = [sos_idx]
-        last_attention = None
-
-        for i in range(max_trg_len):
-
-            # Get predicted words (all)
-            trg_tensor = torch.LongTensor(trg_indexes).unsqueeze(0).to(self.device)  # (1, 1->L)
-
-            # Build target mask
-            trg_mask = self.make_trg_mask(trg_tensor)  # (B, n_heads, L, L)
-
-            with torch.no_grad():
-                # Inputs: source + current translation
-                output, last_attention = self.decoder(trg_tensor, enc_src, trg_mask, src_mask)  # (B, L, output_dim)
-
-            # Get predicted token
-            # Get maximums of the last dimensions (2). Then, for each batch (:), get last word (-1)
-            # => We already "know" the other words since they're the ones we fed
-            pred_token = output.argmax(2)[:, -1].item()
-            trg_indexes.append(pred_token)
-
-            # If predicted token == <eos> => stop
-            if pred_token == eos_idx:
-                break
-
-        return trg_indexes, last_attention
-
-    def translate_sentence(self, src, max_trg_len=50, beam_width=1):
-        # Single sentence ("unlimited length")
-
-        # Get special indices
-        sos_idx = self.trg_field.vocab.stoi[self.trg_field.init_token]
-        eos_idx = self.trg_field.vocab.stoi[self.trg_field.eos_token]
-
-        # Build source mask
-        src_mask = self.make_src_mask(src)
-
-        # Run encoder
-        with torch.no_grad():
-            enc_src = self.encoder(src, src_mask)
-
-        # Set fist word (<sos>)
-        trg_indexes = [([sos_idx], [1.0])]  # Common starting point
-        for t in range(max_trg_len):
-            # Get top-k next token. None if ended
-            top_k_beam = search.get_top_tokens(self, (enc_src, src_mask), trg_indexes, beam_width, eos_idx, max_trg_len)
-
-            if top_k_beam is None:
-                break
-            else:
-                new_top_k = search.greedy_decoding(trg_indexes, top_k_beam)
-                new_trg_indexes = search.update_top_k(trg_indexes, new_top_k, beam_width, eos_idx)
-                trg_indexes = new_trg_indexes
-
-        return trg_indexes, []
-
     def decode_word(self, enc_src, src_mask, trg_indexes):
             # Get predicted words (all)
             trg_tensor = torch.LongTensor(trg_indexes).unsqueeze(0).to(self.device)  # (1, 1->L)
@@ -390,26 +319,55 @@ class Seq2Seq(nn.Module):
 
             # Get last word
             probs, idxs = probs[:, -1, :], idxs[:, -1, :]
-            return probs, idxs
+            return (probs, idxs), last_attention
 
-    def translate(self, sentence, max_length=100):
+    def translate_sentence(self, src, max_trg_len, beam_width):
+        # Single sentence ("unlimited length")
+
+        # Get special indices
+        sos_idx = self.trg_field.vocab.stoi[self.trg_field.init_token]
+        eos_idx = self.trg_field.vocab.stoi[self.trg_field.eos_token]
+
+        # Build source mask
+        src_mask = self.make_src_mask(src)
+
+        # Run encoder
+        with torch.no_grad():
+            enc_src = self.encoder(src, src_mask)
+
+        # Set fist word (<sos>)
+        trg_indexes = [([sos_idx], [1.0], None)]  # Common starting point (indices, probs, attn)
+        for t in range(max_trg_len):
+            # Get top-k next token. None if ended
+            top_k_beam, last_attention = search.get_top_tokens(self, (enc_src, src_mask), trg_indexes, beam_width, eos_idx, max_trg_len)
+
+            if all([x is None for x in top_k_beam]):
+                break
+            else:
+                if beam_width == 1:
+                    new_top_k = search.greedy_search(trg_indexes, top_k_beam, last_attention)
+                else:
+                    new_top_k = search.beam_search(trg_indexes, top_k_beam, beam_width, last_attention)
+                new_trg_indexes = search.update_top_k(trg_indexes, new_top_k, beam_width, eos_idx)
+                trg_indexes = new_trg_indexes
+
+        return trg_indexes
+
+    def translate(self, sentence, max_length, beam_width):
         # Process sentence
         src_preprocessed = self.src_field.preprocess(sentence)
         src_indexes = self.src_field.process([src_preprocessed], self.device)
 
-        trg_indexes, attention = self.translate_sentence(src_indexes, max_trg_len=max_length)
+        trg_pack = self.translate_sentence(src_indexes, max_trg_len=max_length, beam_width=beam_width)
 
         # Convert to CPU
         src_indexes = src_indexes.cpu().int().flatten()
-        # trg_indexes = trg_indexes  # It's a list
-        attention = attention.cpu()
 
         # Convert predicted indices to tokens
         src_tokens = [self.src_field.vocab.itos[int(i)] for i in src_indexes]
-        trg_pred_tokens = [self.trg_field.vocab.itos[int(i)] for i in trg_indexes]
+        trg_pred_tokens, attns = [], []
+        for trg in trg_pack:
+            trg_pred_tokens.append([self.trg_field.vocab.itos[int(i)] for i in trg[0]])
+            attns.append(trg[2].cpu() if trg[2] is not None else None)  # Get attention
 
-        # Process <sos> / <eos>
-        # src_tokens = src_tokens  # Keep <sos> and <eos> as reference
-        trg_pred_tokens = trg_pred_tokens[1:]  # Remove <sos> since it's given
-
-        return (src_tokens, trg_pred_tokens), attention
+        return (src_tokens, trg_pred_tokens), attns
--- seq2seq/transformers/test.py
+++ seq2seq/transformers/test.py
@@ -1,14 +1,101 @@
 
 
+import random
+import time
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torchtext import data
+from torch.utils.tensorboard import SummaryWriter
+
+from seq2seq.transformers import helpers
+from seq2seq import utils
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+# import seaborn as sns
+# sns.set() Problems with attention
+
+import re
+
+import spacy
+nlp_en = spacy.load("en")
+nlp_es = spacy.load("es")
 
 
+# Names
+EXPERIMENT_NAME = "runs/transformer"
+MODEL_NAME = "simple_transformer"
 
-###########################################################################
-###########################################################################
+# Build model and initialize
+DATASET_NAME = "miguel"  # multi30k, miguel
+DATASET_PATH = f"../.data/{DATASET_NAME}"
+ALLOW_DATA_PARALLELISM = False
+MAX_SRC_LENGTH = 100 + 2  # Doesn't include <sos>, <eos>
+MAX_TRG_LENGTH = 100 + 2  # Doesn't include <sos>, <eos>
+MAX_TRG_LENGTH_TEST = int(MAX_TRG_LENGTH * 1.0)  # len>1.0 is not supported by all models
+CHECKPOINT_PATH = f'checkpoints/simple_transformer_2.pt'
+SOS_WORD = '<sos>'
+EOS_WORD = '<eos>'
+
+# Set fields
+SRC = data.Field(tokenize='spacy', tokenizer_language="en", init_token=SOS_WORD, eos_token=EOS_WORD, lower=True, batch_first=True)
+TRG = data.Field(tokenize='spacy', tokenizer_language="es", init_token=SOS_WORD, eos_token=EOS_WORD, lower=True, batch_first=True)
+fields = [('src', SRC), ('trg', TRG)]
+
+# Load vocabulary
+src_vocab = utils.load_vocabulary(f'{DATASET_PATH}/tokenized/src_vocab.pkl')
+trg_vocab = utils.load_vocabulary(f'{DATASET_PATH}/tokenized/trg_vocab.pkl')
+print("Vocabularies loaded!")
+
+# Add vocabularies to fields
+SRC.vocab = src_vocab
+TRG.vocab = trg_vocab
+print(f"Unique tokens in source (en) vocabulary: {len(SRC.vocab)}")
+print(f"Unique tokens in target (es) vocabulary: {len(TRG.vocab)}")
 
 
+# Set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(utils.gpu_info())
 
-###########################################################################
-###########################################################################
+
+from seq2seq.models import s2s_6_transfomer as builder
+model = builder.make_model(src_field=SRC, trg_field=TRG,
+                           max_src_len=MAX_SRC_LENGTH, max_trg_len=MAX_TRG_LENGTH, device=device,
+                           data_parallelism=ALLOW_DATA_PARALLELISM)
+
+# Load best model
+model.load_state_dict(torch.load(CHECKPOINT_PATH))
+print("Model loaded!")
+
+# Set for evaluation
+model.eval()
+
+# Translate
+src_sentence = "Barack Hussein Obama II is an American politician and attorney who served as the 44th president of the United States of America from 2009 to 2017."
+
+# Beam 1
+(src_tokens, trans_tokens), attns1 = model.translate(src_sentence, max_length=MAX_TRG_LENGTH_TEST, beam_width=1)
+utils.show_translation_pair(src_tokens, trans_tokens, nlp_src=nlp_en, nlp_trg=nlp_es)
+
+# Beam 3
+(src_tokens, trans_tokens), attns3 = model.translate(src_sentence, max_length=MAX_TRG_LENGTH_TEST, beam_width=3)
+utils.show_translation_pair(src_tokens, trans_tokens, nlp_src=nlp_en, nlp_trg=nlp_es)
+
+
+# Plot attention
+head_i = 5
+attn = attns1[0][0]  # Remove Beam and Batch
+utils.display_attention(src_tokens, trans_tokens[0], attn[head_i], title=f"Attention (Head #{head_i})")
+
+# Plot attention
+head_i = 5
+attn = attns3[0][0]  # Remove Beam and Batch
+utils.display_attention(src_tokens, trans_tokens[0], attn[head_i], title=f"Attention (Head #{head_i})")
 
 print("Done!")
