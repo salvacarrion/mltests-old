02512fab3160fdc10c91792e7ad446e06c2cd057
Salva Carrión <salva.carrion@outlook.com>
2021-02-01 23:29:15 +0100

    Update fairseq evaluate

--- paper1/code/fairseq/5_evaluate.py
+++ paper1/code/fairseq/5_evaluate.py
@@ -6,11 +6,9 @@ if os.environ.get('MACHINE') == "HOME":
 else:
     BASE_PATH = "/home/scarrion/datasets/Scielo/fairseq/"
 
-VOCAB_SIZE = 32000
-
 for SRC_LANG, TRG_LANG in [("es", "en"), ("pt", "en")]:
     for d in ["health", "biological", "merged"]:
         for model in ["transformer/"]:
             dataset = f"scielo_{d}_{SRC_LANG}_{TRG_LANG}"
             path = os.path.join(BASE_PATH, dataset)
-            subprocess.call(['sh', './scripts/5_evaluate.sh', SRC_LANG, TRG_LANG, model, dataset, path])
+            subprocess.call(['sh', './scripts/5_evaluate.sh', SRC_LANG, TRG_LANG, model+"checkpoint_best.pt", dataset, path])
