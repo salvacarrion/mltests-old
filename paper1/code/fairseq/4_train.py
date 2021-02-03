69ae65bbf89fc62179c9cc49ed32d168fa55c7af
salvacarrion <salva.carrion@outlook.com>
2021-02-03 01:44:28 +0100

    Update scripts

--- paper1/code/fairseq/4_train.py
+++ paper1/code/fairseq/4_train.py
@@ -9,7 +9,7 @@ else:
 VOCAB_SIZE = 32000
 
 for SRC_LANG, TRG_LANG in [("es", "en"), ("pt", "en")]:
-    for d in ["biological"]:
+    for d in ["health", "biological", "merged"]:
         dataset = f"scielo_{d}_{SRC_LANG}_{TRG_LANG}"
         path = os.path.join(BASE_PATH, dataset)
         subprocess.call(['sh', './scripts/4_train.sh', SRC_LANG, TRG_LANG, path])
