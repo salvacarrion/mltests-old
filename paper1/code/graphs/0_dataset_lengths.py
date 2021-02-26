f3b0ddca49668d13d262a35999c8e00008faf7c0
salvacarrion <salva.carrion@outlook.com>
2021-02-26 17:24:53 +0100

    Add graphs + update jupyter

---
+++ paper1/code/graphs/0_dataset_lengths.py
@@ -0,0 +1,58 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+BASE_PATH = "/home/salvacarrion/Documents/Programming/Datasets/Scielo/fairseq/"
+
+DATASETS = [
+    "scielo_biological_es_en",
+    "scielo_biological_pt_en",
+    "scielo_health_biological_es_en",
+    "scielo_health_biological_pt_en",
+    "scielo_health_es_en",
+    "scielo_health_pt_en",
+    "scielo_merged_es_en",
+    "scielo_merged_pt_en"
+]
+
+rows = []
+for dataset in DATASETS:
+
+    for split in ["train", "val", "test"]:
+        filename = f"{dataset}/raw/{split}.en"
+        with open(os.path.join(BASE_PATH, filename), 'r') as f:
+            length = len(f.read().strip().split('\n'))
+            text = dataset.replace("scielo_", "").replace("_", "-")
+            rows.append([text, split, length])
+
+# Save sizes
+df = pd.DataFrame(rows, columns=["Dataset", "Split", "Size"])
+df.to_csv(f"../../data/new/split_sizes.csv")
+print("Data saved!")
+
+# Print sizes
+print(df)
+
+# Draw a nested barplot by species and sex
+g = sns.catplot(data=df, x="Dataset", y="Size", kind="bar", hue="Split", legend=False)
+g.fig.set_size_inches(12, 8)
+
+# properties
+g.set(xlabel='Datasets', ylabel='Sizes')
+plt.title(f"Sizes across splits")
+
+g.set_xticklabels(rotation=45, horizontalalignment="center")
+plt.legend(loc='upper right')
+plt.tight_layout()
+
+# Save figure
+plt.savefig(f"../../data/new/images/split_sizes.pdf")
+plt.savefig(f"../../data/new/images/split_sizes.jpg")
+print("Figure saved!")
+
+# Show plot
+plt.show()
+asd = 3
